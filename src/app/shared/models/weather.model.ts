449b65c5534b6ef9892d134787ffd109f409d760
thothdragon <thothdragons@gmail.com>
2020-02-13 16:25:17 +0100

    Prepare new WeatherModel

--- src/app/shared/models/weather.model.ts
+++ src/app/shared/models/weather.model.ts
@@ -6,3 +6,69 @@ export class Weather {
     public wind: number;
     public temperature: Temperature;
 }
+
+/*
+declare module namespace {
+
+  export interface Coord {
+      lon: number;
+      lat: number;
+  }
+
+  export interface Weather {
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+  }
+
+  export interface Main {
+      temp: number;
+      feels_like: number;
+      temp_min: number;
+      temp_max: number;
+      pressure: number;
+      humidity: number;
+  }
+
+  export interface Wind {
+      speed: number;
+      deg: number;
+      gust: number;
+  }
+
+  export interface Rain {
+      3h: number;
+  }
+
+  export interface Clouds {
+      all: number;
+  }
+
+  export interface Sys {
+      type: number;
+      id: number;
+      country: string;
+      sunrise: number;
+      sunset: number;
+  }
+
+  export interface RootObject {
+      coord: Coord;
+      weather: Weather[];
+      base: string;
+      main: Main;
+      visibility: number;
+      wind: Wind;
+      rain: Rain;
+      clouds: Clouds;
+      dt: number;
+      sys: Sys;
+      timezone: number;
+      id: number;
+      name: string;
+      cod: number;
+  }
+
+}
+*/
