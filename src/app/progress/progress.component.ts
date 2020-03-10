8e5f648a6bb26e7988e0a2654b06609e329583e7
thothdragon <thothdragons@gmail.com>
2020-03-10 14:25:15 +0100

    Unactiv ProgressBar visual when only cityName

--- src/app/progress/progress.component.ts
+++ src/app/progress/progress.component.ts
@@ -5,6 +5,7 @@ import { WeatherService } from '../shared/services/weather.service';
 import { CityService } from '../shared/services/city.service';
 import { Weather } from '../shared/models/weather.model';
 import { Forecast } from '../shared/models/forecast.model';
+import { PositionService } from '../shared/services/position.service';
 
 @Component({
   selector: 'app-progress',
@@ -18,35 +19,45 @@ export class ProgressComponent implements AfterViewInit {
   value = 50;
   bufferValue = 75;
   progress: boolean = true;
-  cityName: string;
-  weather: Weather;
-  forecast: Forecast
 
   constructor(
     private weatherService: WeatherService,
     private cityService: CityService,
+    private positionService: PositionService,
   ) { }
 
   ngAfterViewInit(): void {
 
-    this.cityService.getCity().subscribe(
-      (e) => {
-        this.cityName = e;
-        this.weatherService.getWeatherByName(this.cityName).subscribe(
-          (e) => {
-            this.weather = e
-            this.weatherService.getForecastByName(this.cityName).subscribe(
-              (e) => {
-                this.forecast = e;
-                if (this.cityName && (this.weather && this.forecast)) {
-                  this.progress = false;
-                  console.log(this.progress);
-                }
+    this.positionService.getLongitude()
+      .subscribe(
+        (longitude: number) => {
+          this.positionService.getLatitude()
+            .subscribe(
+              (latitude: number) => {
+                this.weatherService.getWeatherByCoords(longitude, latitude)
+                  .subscribe((weather: Weather) => {
+                    this.weatherService.getForecastByCoords(longitude, latitude)
+                      .subscribe((forecast: Forecast) => {
+                        this.progress = false;
+                      })
+                  })
               }
             );
-          }
-        );
-      }
-    );
+        }
+      );
+
+    //   this.cityService.getCity().subscribe(
+    //     (cityName: string) => {
+    //       this.weatherService.getWeatherByName(cityName).subscribe(
+    //         (weather: Weather) => {
+    //           this.weatherService.getForecastByName(cityName).subscribe(
+    //             (forecast: Forecast) => {
+    //               this.progress = false;
+    //             }
+    //           );
+    //         }
+    //       );
+    //     }
+    //   );
   }
 }
